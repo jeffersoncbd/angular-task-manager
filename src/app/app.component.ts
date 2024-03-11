2504727d2f147bf1e25d7f7c1cb87541c928543c
Jefferson Carlos <jeff.carlosbd@gmail.com>
2024-03-10 22:18:40 -0300

    atualiza readme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { GithubIconComponent } from './github-icon/github-icon.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, GithubIconComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
---
+++ src/app/github-icon/github-icon.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-github-icon',
+  standalone: true,
+  imports: [],
+  templateUrl: './github-icon.component.svg',
+  styleUrl: './github-icon.component.css',
+})
+export class GithubIconComponent {}
